135d390b40ba24845e7b7cdc3115f26e24a87ecc
Anton Struyk <astruyk@gmail.com>
2018-03-23 11:14:32 -0400

    Initial plugin metafile.

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-private-tag-plugin
+# about: A plugin for Discourse that adds the ability to hide posts from the public feed using tags.
+# version: 1.0
+# authors: Anton Struyk
+# url: https://github.com/astruyk/discourse-private-tag-plugin
